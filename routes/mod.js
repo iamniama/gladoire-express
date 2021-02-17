7da4505b00035b8bdc2d694be14236524259d050
iamniama <plectrumonthespectrum@gmail.com>
2021-02-17 13:06:40 -0700

    added flash error for unauthorized mod access attempt with redirect to homepage (instead of the FBI's cybersecurity site)

--- routes/mod.js
+++ routes/mod.js
@@ -16,12 +16,18 @@ router.get('/', isLoggedIn, (req, res)=>{
 router.get('/info', isLoggedIn, async(req,res)=>{
     if (req.user.user_level >= 5){
         res.render('mod/infolist', {data:{user: req.user, items:await db.doc.findAll()}})
+    }else {
+        req.flash('error', 'You do not have moderator access')
+        res.redirect('/')
     }
 })
 
 router.get('/info/new', isLoggedIn, async(req,res)=>{
     if (req.user.user_level >= 5){
         res.render('mod/newinfo', {data: {user: req.user} })
+    }else {
+        req.flash('error', 'You do not have moderator access')
+        res.redirect('/')
     }
 })
 
@@ -29,6 +35,9 @@ router.post('/info', isLoggedIn, async(req, res)=>{
     if (req.user.user_level >= 5) {
         db.doc.create(req.body)
         res.redirect('/mod/info/')
+    }else {
+        req.flash('error', 'You do not have moderator access')
+        res.redirect('/')
     }
 })
 
@@ -36,8 +45,9 @@ router.post('/info', isLoggedIn, async(req, res)=>{
 router.get('/info/:id', isLoggedIn, async(req, res)=>{
     if (req.user.user_level >= 5) {
         res.render('mod/edit', {data: {user: req.user, item: await db.doc.findOne({where:{'id': req.params.id}})}})
-    }else{
-        res.redirect(redirSite)
+    }else {
+        req.flash('error', 'You do not have moderator access')
+        res.redirect('/')
     }
 })
 
@@ -48,6 +58,9 @@ router.put('/info', isLoggedIn, async(req,res)=>{
         console.log(req.body)
         db.doc.update({doc_name: req.body.doc_name, doc_url: req.body.doc_url}, { where: {id: req.body.id}})
         res.redirect(`/mod/info/${req.body.id}`)
+    }else {
+        req.flash('error', 'You do not have moderator access')
+        res.redirect('/')
     }
 })
 
