217eaaca8ea7fbb0267c84b0175651097c3ef69a
iamniama <plectrumonthespectrum@gmail.com>
2021-02-20 21:47:22 -0700

    MVP: removed debug logs for moon data and timestamp

--- routes/entries.js
+++ routes/entries.js
@@ -28,9 +28,7 @@ router.get('/', isLoggedIn, async(req,res)=>{
 router.get('/new', isLoggedIn, async(req, res)=>{
     try {
         let utime = Math.floor((new Date()).getTime() / 1000)
-        console.log(`********************TIMESTAMP FOR TESTING${utime}**********************`)
         let mdata = (await axios.get(`https://api.farmsense.net/v1/moonphases/?d=${utime}`)).data
-        console.log(`******************MOON DATA: ${mdata}*********************`)
         let catinfo = await db.category.findAll({
             include: [
                 {
