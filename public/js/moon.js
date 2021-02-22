c56f856c56469e8bc0ca79e43ac8daeeea9e8b13
iamniama <plectrumonthespectrum@gmail.com>
2021-02-21 17:56:21 -0700

    added client side js to update sess_moon when sess_date changes

--- public/js/moon.js
+++ public/js/moon.js
@@ -1,9 +1,5 @@
 document.querySelector('#sess_date').addEventListener('change', (evt)=>{
-    //alert((document.querySelector('#sess_date').valueAsDate).getTime() / 1000)
     let tstamp = document.querySelector('#sess_date').value
-    //alert()
-    //let tstamp = Math.floor((document.querySelector('#sess_date').valueAsDate).getTime() / 1000)
-    alert(`DEBUG: ${tstamp}`)
     let mdata = fetch(`https://api.farmsense.net/v1/moonphases/?d=${(new Date(tstamp)).getTime() / 1000}`)
         .then(response => response.json())
         .then((mdata)=>{
