ce1ddc950938a9117e1734e4428c38751e9c6da1
iamniama <plectrumonthespectrum@gmail.com>
2021-02-21 17:42:37 -0700

    hrrrrrrrr

--- public/js/moon.js
+++ public/js/moon.js
@@ -1,6 +1,6 @@
 document.querySelector('#sess_date').addEventListener('change', (evt)=>{
     //alert((document.querySelector('#sess_date').valueAsDate).getTime() / 1000)
-    let tstamp = document.querySelector('#sess_date').value
+    let tstamp = document.querySelector('#sess_date').getDate()
     alert(tstamp.getTime())
     //let tstamp = Math.floor((document.querySelector('#sess_date').valueAsDate).getTime() / 1000)
     alert(`DEBUG: ${tstamp}`)
