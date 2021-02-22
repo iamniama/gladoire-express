25404b3ed193d33b99367fc19c22337cbdf2f310
iamniama <plectrumonthespectrum@gmail.com>
2021-02-21 17:29:17 -0700

    hrrrrrrrr

--- public/js/moon.js
+++ public/js/moon.js
@@ -1,7 +1,7 @@
 document.querySelector('#sess_date').addEventListener('change', (evt)=>{
-    alert("DEBUG")
-    let tstamp = Math.floor(document.querySelector('#sess_date').valueAsDate.getTime() / 1000)
-    alert(tstamp)
+    //alert("DEBUG")
+    let tstamp = Math.floor((document.querySelector('#sess_date').valueAsDate).getTime() / 1000)
+    alert(`DEBUG: ${tstamp}`)
     let mdata = fetch(`https://api.farmsense.net/v1/moonphases/?d=${tstamp}`)
         .then(response => response.json())
         .then((mdata)=>{
